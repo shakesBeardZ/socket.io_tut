8023babf07e3f28c16ba2cd707a100d11cd9a6b3
shakesbeardz <battachyahia@hotmail.com>
2018-10-13 01:38:25 +0200

    namespaces in socket.io

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const server  = require('http').Server(app);
 const io =  require('socket.io')(server);
-const port = 6060
+const port = 6060;
 
 server.listen(port , () => {
     console.log(`server listening on ${port}`);
@@ -10,4 +10,19 @@ server.listen(port , () => {
 app.get('*', (req, res)=>{
     res.sendFile(__dirname + '/public/index.html');
 })
+
+
+const tech = io.of('/tech')
+tech.on("connection", (socket) => {
+    console.log("user connected");
+    socket.on("message" , (data)=> {
+        tech.emit("message", data);
+    })
+
+    socket.on('disconnect' , () => {
+        console.log('user disconnected ');
+        tech.emit('message' , 'user disconnected ')
+    })
+})
+
 
